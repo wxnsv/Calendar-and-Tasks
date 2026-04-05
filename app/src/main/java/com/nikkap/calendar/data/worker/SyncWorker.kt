a57c14307d0106d561db6e8815506f6daacdee6d
wxnsv <nikitaakapitonov@gmail.com>
2026-04-05 14:28:02 +0300

    refactor: do syncing asynchronous in SyncWorker and add retry if sync was failed

--- app/src/main/java/com/nikkap/calendar/data/worker/SyncWorker.kt
+++ app/src/main/java/com/nikkap/calendar/data/worker/SyncWorker.kt
@@ -5,16 +5,20 @@ import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.nikkap.calendar.domain.repository.CalendarRepository
 import com.nikkap.calendar.domain.repository.TaskRepository
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 
 class SyncWorker(
     appContext: Context, params: WorkerParameters,
-    private val calendarRepository: CalendarRepository, private val taskRepository: TaskRepository
-) :
-    CoroutineWorker(appContext, params) {
+    private val calendarRepository: CalendarRepository,
+    private val taskRepository: TaskRepository
+) : CoroutineWorker(appContext, params) {
     override suspend fun doWork(): Result {
-        calendarRepository.syncCalendar()
-        taskRepository.syncTasks()
-
-        return Result.success()
+        return coroutineScope {
+            val calendarResult = async { calendarRepository.syncCalendar() }
+            val taskResult = async { taskRepository.syncAllTasks() }
+            if (calendarResult.await().isSuccess && taskResult.await().isSuccess) Result.success()
+            else Result.retry()
+        }
     }
 }
