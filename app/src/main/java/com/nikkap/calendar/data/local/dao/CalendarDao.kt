dee849c48c9dcf55a548ffbc1dc9fd7004b2f836
wxnsv <nikitaakapitonov@gmail.com>
2026-04-05 13:34:01 +0300

    refactor: grouped the methods in dao by type of entity, added funcs for bulk receipt or deletion of entities

--- app/src/main/java/com/nikkap/calendar/data/local/dao/CalendarDao.kt
+++ app/src/main/java/com/nikkap/calendar/data/local/dao/CalendarDao.kt
@@ -1,6 +1,7 @@
 package com.nikkap.calendar.data.local.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
@@ -11,36 +12,64 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface CalendarDao {
-    @Query("SELECT * FROM event")
-    fun getAllEvents(): Flow<List<EventEntity>>
 
-    @Query("SELECT * FROM birthday")
-    fun getAllBirthdays(): Flow<List<BirthdayEntity>>
+    /**
+     * EVENTS
+     */
+
+    @Query("SELECT * FROM event WHERE pendingAction != 'DELETE'")
+    fun getNonDeleteEvents(): Flow<List<EventEntity>>
+
+    @Query("SELECT * FROM event WHERE pendingAction != 'NONE'")
+    fun getPendingEvents(): Flow<List<EventEntity>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertEvents(eventEntities: List<EventEntity>)
 
+    @Delete
+    suspend fun deleteEvent(eventEntity: EventEntity)
+
+    @Query("SELECT * from event WHERE id = :id")
+    suspend fun getEvent(id: String): EventEntity?
+
+    @Query("DELETE FROM event WHERE id IN (:ids)")
+    suspend fun deleteEventsByIds(ids: List<String>)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertEvent(eventEntity: EventEntity)
+
+    @Update
+    suspend fun updateEvent(eventEntity: EventEntity)
+
+    /**
+     * BIRTHDAYS
+     */
+
+    @Query("SELECT * FROM birthday WHERE pendingAction != 'DELETE'")
+    fun getNonDeleteBirthdays(): Flow<List<BirthdayEntity>>
+
+    @Query("SELECT * FROM birthday WHERE pendingAction != 'NONE'")
+    fun getPendingBirthdays(): Flow<List<BirthdayEntity>>
+
+    @Query("DELETE FROM birthday")
+    suspend fun clearBirthdays()
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertBirthdays(birthdayEntities: List<BirthdayEntity>)
 
-    @Query("SELECT COUNT(*) FROM event")
-    suspend fun getCount(): Int
+    @Delete
+    suspend fun deleteBirthday(birthdayEntity: BirthdayEntity)
 
-    @Query("SELECT * from event WHERE id = :id")
-    suspend fun getEvent(id: String): EventEntity
+    @Query("DELETE FROM birthday WHERE id IN (:ids)")
+    suspend fun deleteBirthdaysByIds(ids: List<String>)
 
     @Query("SELECT * from birthday WHERE id = :id")
     suspend fun getBirthday(id: String): BirthdayEntity
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertEvent(eventEntity: EventEntity)
-
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertBirthday(birthdayEntity: BirthdayEntity)
 
     @Update
-    suspend fun updateEvent(eventEntity: EventEntity)
-
-    @Update
     suspend fun updateBirthday(birthdayEntity: BirthdayEntity)
+
 }
--- app/src/main/java/com/nikkap/calendar/data/local/dao/TaskDao.kt
+++ app/src/main/java/com/nikkap/calendar/data/local/dao/TaskDao.kt
@@ -1,6 +1,7 @@
 package com.nikkap.calendar.data.local.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
@@ -12,21 +13,16 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TaskDao {
+
+    /**
+     * TASKS
+     */
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertTasks(tasks: List<TaskEntity>)
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertSubtasks(tasks: List<SubtaskEntity>)
-    @Query("DELETE FROM tasks")
-    suspend fun clearTasks()
-    @Query("SELECT COUNT(*) FROM tasks")
-    suspend fun getCount(): Int
-
     @Query("SELECT * from tasks WHERE id = :id")
     suspend fun getTask(id: String): TaskEntity
-
-    @Query("SELECT * from subtasks WHERE id = :id")
-    suspend fun getSubtask(id: String): SubtaskEntity
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertTask(taskEntity: TaskEntity)
@@ -34,15 +30,71 @@ interface TaskDao {
     @Update
     suspend fun updateTask(taskEntity: TaskEntity)
 
+    @Delete
+    suspend fun deleteTask(taskEntity: TaskEntity)
+
+    @Query("DELETE FROM tasklist WHERE id IN (:ids)")
+    suspend fun deleteTaskListsByIds(ids: List<String>)
+
+    @Query("DELETE FROM tasks WHERE id IN (:ids)")
+    suspend fun deleteTasksByIds(ids: List<String>)
+
+    @Query("SELECT * FROM tasks WHERE pendingAction != 'DELETE'")
+    fun getNonDeleteTasks(): Flow<List<TaskEntity>>
+
+    @Query("SELECT * FROM tasks WHERE pendingAction != 'NONE'")
+    fun getPendingTasks(): Flow<List<TaskEntity>>
+
+    /**
+     * SUBTASKS
+     */
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertSubtasks(tasks: List<SubtaskEntity>)
+
+    @Query("DELETE FROM subtasks")
+    suspend fun clearSubtasks()
+
+    @Query("SELECT * from subtasks WHERE id = :id")
+    suspend fun getSubtask(id: String): SubtaskEntity
+
+    @Query("DELETE FROM subtasks WHERE id IN (:ids)")
+    suspend fun deleteSubtasksByIds(ids: List<String>)
+
+    @Query("SELECT * FROM subtasks WHERE pendingAction != 'DELETE'")
+    fun getNonDeleteSubtasks(): Flow<List<SubtaskEntity>>
+
+    @Query("SELECT * FROM subtasks WHERE pendingAction != 'NONE'")
+    fun getPendingSubtasks(): Flow<List<SubtaskEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertSubtask(subtaskEntity: SubtaskEntity)
+
+    @Update
+    suspend fun updateSubtask(subtaskEntity: SubtaskEntity)
+
+    @Delete
+    suspend fun deleteSubtask(subtaskEntity: SubtaskEntity)
+
+    /**
+     * TASKLISTS
+     */
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertTaskLists(taskLists: List<TaskListEntity>)
 
-    @Query("SELECT * from tasklist")
-    fun getTaskLists(): Flow<List<TaskListEntity>>
+    @Query("SELECT * from tasklist WHERE pendingAction != 'DELETE'")
+    fun getNonDeleteTaskLists(): Flow<List<TaskListEntity>>
 
-    @Query("SELECT * FROM tasks WHERE pendingAction != 'DELETE'")
-    fun getAllTasks(): Flow<List<TaskEntity>>
+    @Query("SELECT * from tasklist WHERE pendingAction != 'NONE'")
+    fun getPendingTaskLists(): Flow<List<TaskListEntity>>
 
-    @Query("SELECT * FROM subtasks WHERE pendingAction != 'DELETE'")
-    fun getAllSubtasks(): Flow<List<SubtaskEntity>>
+    @Update
+    suspend fun updateTasklist(taskList: TaskListEntity)
+
+    @Delete
+    suspend fun deleteTasklist(taskList: TaskListEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertTaskList(taskList: TaskListEntity)
+
 }
